5842c5c12f71d15fc4f05002eda2530819f03633
Pete Smith <roysvork@gmail.com>
2014-02-23 13:49:42 +0000

    Adding project files

---
+++ Owin.Context/ContextBuilder.cs
@@ -0,0 +1,46 @@
+﻿namespace Owin.Context
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ContextBuilder<T> : IContextBuilder<T>
+    {
+        private readonly IAppBuilder baseAppBuilder;
+
+        public ContextBuilder(IAppBuilder baseAppBuilder, T context)
+        {
+            this.baseAppBuilder = baseAppBuilder;
+            this.Context = context;
+        }
+
+        public IAppBuilder Use(object middleware, params object[] args)
+        {
+            return this.baseAppBuilder.Use(middleware, args);
+        }
+
+        public object Build(Type returnType)
+        {
+            return this.baseAppBuilder.Build(returnType);
+        }
+
+        public IAppBuilder New()
+        {
+            return this.baseAppBuilder.New();
+        }
+
+        public IDictionary<string, object> Properties
+        {
+            get
+            {
+                return baseAppBuilder.Properties;
+            }
+        }
+
+        public T Context { get; set; }
+    }
+
+    public interface IContextBuilder<T> : IAppBuilder
+    {
+        T Context { get; set; }
+    }
+}
---
+++ Owin.Context/Extensions.cs
@@ -0,0 +1,17 @@
+namespace Owin.Context
+{
+    using System;
+
+    public static class Extensions
+    {
+        public static IContextBuilder<T> Context<T>(this IAppBuilder app, Func<IContextBuilder<T>, IContextBuilder<T>> setup) where T : new()
+        {
+            return setup(new ContextBuilder<T>(app, new T()));
+        }
+
+        public static IContextBuilder<T> Use<T>(this IContextBuilder<T> builder, Func<IAppBuilder, T, IAppBuilder> setup)
+        {
+            return setup(builder, builder.Context) as IContextBuilder<T>;
+        }
+    }
+}
